10f04ce30845741f88143bf209694080d7376cf3
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-03-29 15:02:26 -0400

    Adding init doc methods

--- docs/conf.py
+++ docs/conf.py
@@ -159,3 +159,6 @@ texinfo_documents = [
 
 
 # -- Extension configuration -------------------------------------------------
+# autodoc __init__ methods
+# https://stackoverflow.com/questions/5599254/how-to-use-sphinxs-autodoc-to-document-a-classs-init-self-method
+autoclass_content = 'both'
