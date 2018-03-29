07c8b3ed5f9ee24b0b658a3aa2dbb22252ed004d
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-03-29 14:50:31 -0400

    Adding better documented class functions. .. path is required

--- sshlog/hello.py
+++ sshlog/hello.py
@@ -2,10 +2,29 @@ class HelloWorld:
     """ HelloWorld class will tell you hello!
 
     """
+    def __init__(self, firstname, lastname):
+        """
+        Args:
+           firstname (str): first name of user
+           lastname (str): last name of user
+        """
+        self.firstname = firstname
+        self.lastname = lastname
 
-    def __init__(self):
-        """Hello world docstring to check that works."""
+    @property
+    def hello(self):
+        """ say hello to user
+        Returns:
+            str: special hello message to user
+        """
+        return f'Hello {self.firstname} {self.lastname}'
 
-    def hello(name):
-        """say hello"""
-        print(f'Hello {name}!')
+    def helloworld(self, name):
+        """string with special hello message to name
+        Args:
+            name (str): the person to say hello to
+
+        Returns:
+            str: special hello world message
+        """
+        return f'Hello World {self.name}!'
