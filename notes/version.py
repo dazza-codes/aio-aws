de6bf775976eb8700464931d2c704883c824e173
Darren Weber <dweber.consulting@gmail.com>
2020-01-02 12:48:00 -0800

    Adapt template for python-notes project

--- notes/version.py
+++ notes/version.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long
 # fmt: off
 """
 A version module managed by the invoke-release task
--- tasks.py
+++ tasks.py
@@ -6,12 +6,12 @@ from invoke_release.tasks import *  # noqa: F403
 from invoke_release.plugins import PatternReplaceVersionInFilesPlugin
 
 configure_release_parameters(  # noqa: F405
-    module_name="package",
-    display_name="package",
+    module_name="notes",
+    display_name="python-notes",
     # python_directory="package",
     plugins=[
         PatternReplaceVersionInFilesPlugin(
-            "docs/conf.py", "package/version.py", "pyproject.toml", "README.md"
+            "docs/conf.py", "notes/version.py", "pyproject.toml", "README.md"
         )
     ],
 )
