3681e8dd830caf4f53f7ff131eef2dd441127c91
Darren Weber <darren.weber@jupiterintel.com>
2020-01-02 17:29:57 -0800

    Notes for asyncio concurrency

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,19 @@ release = "0.1.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.ifconfig",
+    "IPython.sphinxext.ipython_console_highlighting",
+    "IPython.sphinxext.ipython_directive",
+]
+
+autodoc_default_options = {"members": True, "show-inheritance": True}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
