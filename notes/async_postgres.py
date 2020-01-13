94f04f54ab8baf17b7f9c09cb54a88e10c24e23a
Darren Weber <dweber.consulting@gmail.com>
2020-01-13 08:43:28 -0800

    Update notes for async databases

--- notes/async_postgres.py
+++ notes/async_postgres.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 """
-Dummy code for asyncpg
+Example code for asyncpg
 
 .. seealso::
     - https://github.com/magicstack/asyncpg#basic-usage
@@ -10,24 +10,22 @@ Dummy code for asyncpg
 import asyncpg  # type: ignore
 
 
-async def run():
+async def run_asyncpg():
     """
-    Run dummy async query on PostgreSQL
+    Run example queries on PostgreSQL
 
     :return: database records
     """
     conn = await asyncpg.connect(
         user="user", password="password", database="database", host="127.0.0.1"
     )
-    records = await conn.fetch("""SELECT * FROM mytable""")
+    records = await conn.fetch("SELECT * FROM mytable")
     await conn.close()
     return records
 
 
 if __name__ == "__main__":
 
-    # cannot run without postgres setup
-    # from notes.async_main import main
-    # main(run())
+    from notes.async_main import main
 
-    pass
+    main(run_asyncpg())
