c60090ad841df130a67665c69e74d21857b900d5
Darren Weber <dweber.consulting@gmail.com>
2020-03-29 15:52:11 -0700

    Released aio-aws version 0.2.0
    Changelog Details:
    Focus on aio-aws

--- aio_aws/version.py
+++ aio_aws/version.py
@@ -8,6 +8,6 @@ See also tasks.py
 
 from __future__ import unicode_literals
 
-__version_info__ = (0, 1, 0)
+__version_info__ = (0, 2, 0)
 __version__ = '-'.join(filter(None, ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]]))
 # fmt: on
