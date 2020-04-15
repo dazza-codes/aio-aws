2ff3f6d0548e6d8d00f89a251bee71e23a5c173f
Darren Weber <dweber.consulting@gmail.com>
2020-04-14 19:56:34 -0700

    Fix moto-s3-bucket creation with unique bucket names

--- tests/test_aio_aws_s3.py
+++ tests/test_aio_aws_s3.py
@@ -84,10 +84,11 @@ def aio_s3_object_text() -> str:
 
 
 @pytest.fixture
-async def aio_s3_bucket(aio_s3_uri, aio_aws_s3_client) -> str:
+async def aio_s3_bucket(aio_s3_uri, aio_aws_s3_client, aws_region) -> str:
     s3_parts = S3Parts.parse_s3_uri(aio_s3_uri)
     resp = await aio_aws_s3_client.create_bucket(
-        Bucket=s3_parts.bucket, ACL="public-read-write"
+        Bucket=s3_parts.bucket, ACL="public-read-write",
+        CreateBucketConfiguration={'LocationConstraint': aws_region}
     )
     assert response_success(resp)
     head = await aio_aws_s3_client.head_bucket(Bucket=s3_parts.bucket)
@@ -97,11 +98,14 @@ async def aio_s3_bucket(aio_s3_uri, aio_aws_s3_client) -> str:
 
 
 @pytest.fixture
-async def aio_s3_buckets(aio_s3_bucket_name, aio_aws_s3_client) -> List[str]:
+async def aio_s3_buckets(aio_s3_bucket_name, aio_aws_s3_client, aws_region) -> List[str]:
     bucket_names = []
     for i in range(20):
         bucket_name = f"{aio_s3_bucket_name}_{i:02d}"
-        resp = await aio_aws_s3_client.create_bucket(Bucket=bucket_name)
+        resp = await aio_aws_s3_client.create_bucket(
+            Bucket=bucket_name, ACL="public-read-write",
+            CreateBucketConfiguration={'LocationConstraint': aws_region}
+        )
         assert response_success(resp)
         head = await aio_aws_s3_client.head_bucket(Bucket=bucket_name)
         assert response_success(head)
