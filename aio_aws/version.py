a4e5d56294fd6cc1e473ceb022438e20269dbff8
Darren Weber <dweber.consulting@gmail.com>
2020-11-04 18:50:42 -0800

    Released aio-aws version 0.5.1
    Changelog Details:
    - Minor revisions and black formatting

--- aio_aws/version.py
+++ aio_aws/version.py
@@ -8,6 +8,6 @@ See also tasks.py
 
 from __future__ import unicode_literals
 
-__version_info__ = (0, 5, 0)
+__version_info__ = (0, 5, 1)
 __version__ = '-'.join(filter(None, ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]]))
 # fmt: on
