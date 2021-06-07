36d91d02294b847201fc0f82938a3518be19ee9b
Darren Weber <dweber.consulting@gmail.com>
2021-06-06 18:27:00 -0700

    Released aio-aws version 0.12.0
    Changelog Details:
    - Add lambda accessors using response and data properties
    - Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = "2019-2021, Darren Weber"
 author = "Darren Weber"
 
 # The short X.Y version
-version = "0.11.1"
+version = "0.12.0"
 # The full version, including alpha/beta/rc tags
-release = "0.11.1"
+release = "0.12.0"
 
 
 # -- General configuration ---------------------------------------------------
